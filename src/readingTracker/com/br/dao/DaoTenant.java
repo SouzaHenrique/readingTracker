0b16c6f4c39866d7c6578afaa4816b7b0b95a825
Henrique Martins de Souza <martins_henrique@uninove.edu.br>
2019-05-01 14:20:17 -0300

    Alterações DaoTenant
    Foram implementadas as alterações realizadas na classe DaoTenant pelo Dev Davi L.

--- src/readingTracker/com/br/dao/DaoTenant.java
+++ src/readingTracker/com/br/dao/DaoTenant.java
@@ -1,10 +1,42 @@
 package readingTracker.com.br.dao;
 
+import readingTracker.com.br.factory.ConnectionFactory;
+import readingTracker.com.br.model.*;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DaoTenant implements Dao{
     @Override
     public boolean Save(Object object) {
+
+        PessoaModel oPessoa = null;
+        if(object instanceof PessoaModel){
+            oPessoa = (PessoaModel) object;
+        }else
+        {
+            return false;
+        }
+
+        TenantModel oTenant = new TenantModel();
+        oTenant.setId_pessoa(oPessoa.getId());
+
+        String comando = "insert into Tenant(id_pessoa) values (?)";
+
+        try {
+            PreparedStatement stmt = new ConnectionFactory().getConnection().prepareStatement(comando);
+            stmt.setInt(1, oTenant.getId_pessoa());
+            stmt.execute();
+            return true;
+
+        } catch (SQLException ex) {
+            Logger.getLogger(DaoTenant.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
         return false;
     }
 
@@ -20,11 +52,52 @@ public class DaoTenant implements Dao{
 
     @Override
     public Object get(int id) {
-        return null;
+
+        TenantModel oTenant = null;
+        String comando = "SELECT * FROM tenant where id = ?";
+
+        try{
+            PreparedStatement stmt = new ConnectionFactory().getConnection().prepareStatement(comando);
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+
+            if(rs.next()){
+                oTenant = new TenantModel();
+                rs.getInt("id");
+                rs.getInt("id_pessoa");
+            };
+            return oTenant;
+
+        }catch(SQLException ex){
+            Logger.getLogger(DaoTenant.class.getName()).log(Level.SEVERE, null, ex + "Erro ao executar busca");
+        }
+
+        return false;
     }
 
     @Override
     public List<Object> get() {
+        List<Object> lstTenant = new ArrayList<>();
+        String comando = "SELECT * FROM tenant";
+
+        try{
+            PreparedStatement stmt = new ConnectionFactory().getConnection().prepareStatement(comando);
+            ResultSet rs = stmt.executeQuery();
+
+            while(rs.next()){
+                TenantModel oTenant = new TenantModel(
+                        rs.getInt("id"),
+                        rs.getInt("id_pessoa")
+                );
+                lstTenant.add(oTenant);
+            }
+
+            return lstTenant;
+
+        }catch (SQLException ex){
+            Logger.getLogger(DaoTenant.class.getName()).log(Level.SEVERE, null, ex + "Erro ao executar busca");
+        }
+
         return null;
     }
 }
