cd3a0879e6055429b42e99561a0e34988668e850
Davi Lima <44027077+davilima300@users.noreply.github.com>
2019-05-09 19:13:39 -0300

    Update LeituraController.java

--- src/readingTracker/com/br/controller/LeituraController.java
+++ src/readingTracker/com/br/controller/LeituraController.java
@@ -1,14 +1,18 @@
 package readingTracker.com.br.controller;
 
+import com.google.gson.Gson;
 import readingTracker.com.br.BLL.LeituraBLL;
 import readingTracker.com.br.model.LeituraModel;
-
+import readingTracker.com.br.model.MensagensModel;
+import readingTracker.com.br.model.PessoaModel;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LeituraController extends HttpServlet{
 
@@ -24,32 +28,87 @@ public class LeituraController extends HttpServlet{
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        LeituraModel oLeituraModel = new LeituraModel();
+        LeituraModel oLeitura = new LeituraModel();
         LeituraBLL oLeituraBLL = new LeituraBLL();
-
+        PessoaModel oPessoa = new PessoaModel();
+        MensagensModel msg = new MensagensModel();
         String action = request.getParameter("action");
 
-        //TODO - Davi: Popular oLeituraModel com os parâmetros do request
+        oLeitura.setId_Leitor(Integer.parseInt(request.getParameter("id_leitor")));
+        oLeitura.setId_Livro(Integer.parseInt(request.getParameter("id_livro")));
+        oLeitura.setStatusLeitura(Integer.parseInt(request.getParameter("statusLeitura")));
+        oLeitura.setPaginasLidas(Integer.parseInt(request.getParameter("paginasLidas")));
+        oLeitura.setDataterminoPlanejado(request.getParameter("DataterminoPlanejado"));
+
 
         switch (action){
 
             case "create":{
                 //TODO -  Davi : chamar método BLL para incluir registro de Leitura
+                /*oLeitura.setId_Leitor(Integer.parseInt(request.getParameter("id_leitor")));
+                oLeitura.setId_Livro(Integer.parseInt(request.getParameter("id_livro")));
+                oLeitura.setStatusLeitura(Integer.parseInt(request.getParameter("statusLeitura")));
+                oLeitura.setPaginasLidas(Integer.parseInt(request.getParameter("paginasLidas")));
+                oLeitura.setDataterminoPlanejado(request.getParameter("DataterminoPlanejado"));*/
+
+                if(oLeituraBLL.verificaCampos(oLeitura)){
+                    if(oLeituraBLL.novaLeitura(oLeitura)) {
+                        msg.setMensagem("Leitura cadastrada com sucesso!");
+                    }else{
+                        msg.setMensagem("Não foi possivel cadastrar esta leitura!");
+                    }
+                }else{
+                    msg.setMensagem("Dados invalidos ou nulo!");
+                }
+
+
+                String json = new Gson().toJson(msg);
+                response.setContentType("application/json");
+                response.setCharacterEncoding("UTF-8");
+                response.getWriter().write(json);
                 break;
             }
 
             case "edit":{
                 //TODO - Davi : chamar método BLL para alterar registro de Leitura
+                /*oLeitura.setId_Leitor(Integer.parseInt(request.getParameter("id_leitor")));
+                oLeitura.setId_Livro(Integer.parseInt(request.getParameter("id_livro")));
+                oLeitura.setStatusLeitura(Integer.parseInt(request.getParameter("statusLeitura")));
+                oLeitura.setPaginasLidas(Integer.parseInt(request.getParameter("paginasLidas")));
+                oLeitura.setDataterminoPlanejado(request.getParameter("DataterminoPlanejado"));*/
+
+                if(oLeituraBLL.verificaCampos(oLeitura)){
+                    if (oLeituraBLL.editarLeitura(oLeitura)){
+                    }else{
+                        msg.setMensagem("Não foi possivel editar esta leitura!");
+                    }
+                }else{
+                    msg.setMensagem("Dados invalidos ou nulo!");
+                }
+
+                String json = new Gson().toJson(msg);
+                response.setContentType("application/json");
+                response.setCharacterEncoding("UTF-8");
+                response.getWriter().write(json);
                 break;
             }
 
             case "listar":{
                 //TODO - Davi : chamar método da BLL para listar todas as Leitura
+                List<Object> lstLeitura = new ArrayList<>();
+
+                lstLeitura = oLeituraBLL.listarLeituras(oLeitura.getId_Leitor());
+                String json = new Gson().toJson(lstLeitura);
+                response.setContentType("application/json");
+                response.setCharacterEncoding("UTF-8");
+                response.getWriter().write(json);
+
+
                 break;
             }
 
             case "obter":{
-                // TODO Davi : Chamar méotod da BLL para obter um registro por ID
+
                 break;
             }
 
