5ddf7ea48af53d771fe8f810815d87da81018375
Henrique Martins de Souza <henrique.souza@financas360.com.br>
2019-05-17 21:08:37 -0300

    Removido codigo de teste
    favor nao comitar classe de testes.

--- src/testConsole.java
+++ src/testConsole.java
@@ -16,38 +16,12 @@ public class testConsole {
 
     public static void main(String[] args) {
 
-      LivroModel oLivroModel = new LivroModel();
-      LivroBLL oLivroBLL = new LivroBLL();
 
-      oLivroModel.setTitulo("Harry Potter");
-
-      List<LivroModel> lstLivros = oLivroBLL.buscaLivros(oLivroModel.getTitulo());
-
-      if(!lstLivros.isEmpty()){
-
-          for(LivroModel item : lstLivros){
-              System.out.println(item.getTitulo());
-          }
-
-      }
 
 
 
     }
 
-    private static List<LeituraModel> retListaLeitura(){
-
-        List<LeituraModel> lstLeituras = new ArrayList<LeituraModel>(){
-            {
-                add(new LeituraModel(2,2,3,1,80,"09052019"));
-                add(new LeituraModel(1,2,3,1,80,"09052019"));
-                add(new LeituraModel(3,2,3,1,80,"09052019"));
-                add(new LeituraModel(4,2,3,1,80,"09052019"));
-            }
-        };
-
-        return   lstLeituras;
-    }
 
 
 
